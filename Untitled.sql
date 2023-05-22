650f01be252bc1a6674f2af621739982f4cd6ba6
Derek Berg <108539060+derekberg@users.noreply.github.com>
2023-05-22 09:36:24 -0700

    Add files via upload

---
+++ Untitled.sql
@@ -0,0 +1,16 @@
+with cte as (select cd.continent,
+	   cd.location,
+	   cd.date,
+	   cd.population,
+	   cv.new_vaccinations,
+	   sum(cv.new_vaccinations) over(partition by cd.location
+	   order by cd.location, cd.date) as total_vaccinations
+from coviddeaths cd
+join covidvaccinations cv
+on cd.date = cv.date and cd.location = cv.location
+where cd.continent is not null
+order by 2,3)
+
+select *,
+round(total_vaccinations/population::numeric*100,4)||'%' as percent_vaccinated
+from cte
